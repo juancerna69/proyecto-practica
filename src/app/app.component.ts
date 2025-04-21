486af5d5a23d8df1d5dd5d286628aea49b2a874c
juancerna69 <juancerna715@gmail.com>
2025-04-21 11:04:42 -0300

    nuevo comienzo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgFor,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'car-proyecto';
+  productos=[
+    {nombre :'producto 1',precio :15000},
+    {nombre :'producto 2',precio :20000},
+    {nombre :'producto 3',precio :45000},
+    {nombre :'producto 4',precio :15000},
+    {nombre :'producto 5',precio :15000},
+    {nombre :'producto 6',precio :15000},
+    {nombre :'producto 7',precio :15000},
+  ];
+
 }
